c83243ea2100b6f93f72f4208d665d2acd78226d
VatsalPatel16 <vatsalpatel1180@gmail.com>
2025-02-07 15:50:19 +0530

    Add problem number 217

---
+++ Easy/Problem217_ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package Easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Problem217_ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        // Set to store elements from array and check for duplicate
+        Set<Integer> val = new HashSet<>();
+
+        // Iterate through array nums
+        for (int num : nums) {
+            // if set contains current element that means array contains duplicate
+            // return true in that case
+            if (val.contains(num)) {
+                return true;
+            }
+            // Adding current element in the set
+            val.add(num);
+        }
+        // if not found duplicate then return false
+        return false;
+    }
+}
