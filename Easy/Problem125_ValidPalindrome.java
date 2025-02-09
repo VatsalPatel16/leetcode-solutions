7535bec8b474686a1e9a4a7e903c94dcc3171230
VatsalPatel16 <vatsalpatel1180@gmail.com>
2025-02-09 12:26:00 +0530

    Add problem number 125

---
+++ Easy/Problem125_ValidPalindrome.java
@@ -0,0 +1,31 @@
+package Easy;
+
+// Time Complexity: O(n)
+// Space Complexity: O(n) due to creation of new string
+class Problem125_ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        // regex to remove all non-alphanumeric characters from the string s
+        String regex = "[^a-zA-Z0-9]";
+        // also convert it into lower case
+        s = s.replaceAll(regex, "").toLowerCase();
+        // Two pointers left and right
+        int left = 0, right = s.length() - 1;
+
+        // while both pointers don't cross each other do so
+        while (left < right) {
+            // if charAt left is not equal to charAt right
+            // return false (that means given string is not palindrome)
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            } else {
+                // if equal then increment left pointer and
+                // decrement right pointer
+                left++;
+                right--;
+            }
+        }
+
+        // return true if string is palindrome
+        return true;
+    }
+}
